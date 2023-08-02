88b1a50e7bdeecf7f2877b65befef7108d924b3a
EngEmadHany <emadhany2007@gmail.com>
2023-08-03 00:03:03 +0400

    Initial Commit

---
+++ SwiftUIFormRegistration/SwiftUIFormRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormRegistrationApp.swift
+//  SwiftUIFormRegistration
+//
+//  Created by Emad Isaac on 1/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
